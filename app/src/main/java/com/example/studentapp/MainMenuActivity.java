fa654e14620f98ba0f7b9524e841a899b941fe98
sahyoni122 <mazensahyoni102@gmail.com>
2025-06-12 12:36:44 +0300

    by Sahyoni

--- app/src/main/java/com/example/studentapp/MainMenuActivity.java
+++ app/src/main/java/com/example/studentapp/MainMenuActivity.java
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainMenuActivity extends AppCompatActivity {
 
-    Button btnAddStudent, btnAddCourse, btnLogout, btnBack;
+    Button btnAddStudent, btnAddCourse, btnLogout, btnBack, btnEditUsers, btnViewGrades, btnViewUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +20,24 @@ public class MainMenuActivity extends AppCompatActivity {
         btnAddCourse = findViewById(R.id.btnAddCourse);
         btnLogout = findViewById(R.id.btnLogout);
         btnBack = findViewById(R.id.btnBack);
+        btnEditUsers = findViewById(R.id.btnEditUsers);
+        btnViewGrades = findViewById(R.id.btnViewGrades);
+        btnViewUsers = findViewById(R.id.btnViewUsers);
 
         btnAddStudent.setOnClickListener(v ->
                 startActivity(new Intent(this, AddStudentActivity.class)));
 
         btnAddCourse.setOnClickListener(v ->
                 startActivity(new Intent(this, AddCourseActivity.class)));
+
+        btnEditUsers.setOnClickListener(v ->
+                startActivity(new Intent(this, EditUserActivity.class)));
+
+        btnViewGrades.setOnClickListener(v ->
+                startActivity(new Intent(this, GradesListActivity.class)));
+
+        btnViewUsers.setOnClickListener(v ->
+                startActivity(new Intent(this, UsersListActivity.class)));
 
         btnLogout.setOnClickListener(v -> {
             SharedPreferences prefs = getSharedPreferences("admin_login", MODE_PRIVATE);
@@ -35,7 +47,6 @@ public class MainMenuActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         });
-
 
         btnBack.setOnClickListener(v -> {
             startActivity(new Intent(this, LoginActivity.class));
